32a53734bef5299a76d27f6e612f82e63972572e
June Shim <jys2@sfu.ca>
2019-09-11 01:05:57 +0900

    Cleanup error handling ( .json() )

--- services/error-handler.js
+++ services/error-handler.js
@@ -1,13 +1,9 @@
 module.exports = handleError = (response, error, status) => {
   if (status) {
     console.error(`Error(${status}): ${error}`)
-    return response.status(status).json({
-      error: error
-    })
+    return response.status(status).json(error)
   } else {
     console.error(`Error: ${error}`)
-    return response.json({
-      error: error
-    })
+    return response.json(error)
   }
 }
