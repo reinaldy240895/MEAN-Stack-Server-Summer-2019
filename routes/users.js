30ca61a55fa72721a0c7035b7b8f491f0e71f5d5
June Shim <jys2@sfu.ca>
2019-09-11 16:55:24 +0900

    Add validation, Bug fix: add 'return' to response

--- routes/users.js
+++ routes/users.js
@@ -3,17 +3,31 @@ const User = require('../models/User')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const handleError = require('../services/error-handler')
+// Load input validation
+const validateRegisterInput = require("../validation/register")
+const validateLoginInput = require("../validation/login")
+
 const router = express.Router()
 
 // @route POST api/users/register
 // @desc Register user
 // @access Public
 router.post('/register', (req, resp) => {
+  const {
+    errors,
+    isValid
+  } = validateRegisterInput(req.body)
+
+  // Check validation
+  if (!isValid) {
+    return handleError(resp, errors, 400)
+  }
+
   User.findOne({
     email: req.body.email
   }).then(user => {
     if (user) {
-      handleError(resp, 'Email already exists', 400)
+      return handleError(resp, 'Email already exists', 400)
     } else {
       const user = new User({
         firstname: req.body.firstname,
@@ -25,27 +39,38 @@ router.post('/register', (req, resp) => {
       // Hash password before saving in database
       bcrypt.genSalt(10, (err, salt) => {
         if (err)
-          handleError(resp, err)
+          return handleError(resp, err)
         bcrypt.hash(user.password, salt, (err, hash) => {
           if (err)
-            handleError(resp, err)
+            return handleError(resp, err)
           user.password = hash
           user.save((err, user) => {
             if (err)
-              handleError(resp, err, 400)
-            // handleError(resp, 'Could not create new user', 400)
+              return handleError(resp, err, 400)
+            // return handleError(resp, 'Could not create new user', 400)
             resp.status(200).json(user)
           })
         })
       })
     }
-  }).catch(err => handleError(resp, err))
+  }).catch(err => {
+    return handleError(resp, err)
+  })
 })
 
 // @route POST api/users/login
 // @desc Login user and return JWT token
 // @access Public
 router.post('/login', (req, resp) => {
+  const {
+    errors,
+    isValid
+  } = validateLoginInput(req.body);
+  // Check validation
+  if (!isValid) {
+    return handleError(resp, errors, 400)
+  }
+
   // Form validation
   // const {
   //   errors,
@@ -84,7 +109,7 @@ router.post('/login', (req, resp) => {
           },
           (err, token) => {
             if (err)
-              handleError(resp, err)
+              return handleError(resp, err)
             resp.status(200).json({
               success: true,
               token: 'Bearer ' + token
@@ -92,7 +117,7 @@ router.post('/login', (req, resp) => {
           }
         )
       } else {
-        handleError(resp, 'Password incorrect', 400)
+        return handleError(resp, 'Password incorrect', 400)
         // return resp.status(400).json({
         //   passwordincorrect: 'Password incorrect'
         // })
