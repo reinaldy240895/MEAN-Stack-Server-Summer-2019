46d5e110a7821296ce0418259953459d6c1a4b27
June Shim <jys2@sfu.ca>
2019-09-11 17:15:45 +0900

    Use JSON.stringify(error)

--- services/error-handler.js
+++ services/error-handler.js
@@ -1,9 +1,9 @@
 module.exports = handleError = (response, error, status) => {
   if (status) {
-    console.error(`Error(${status}): ${error}`)
+    console.error(`Error(${status}): ${error instanceof Object ? JSON.stringify(error) : error}`)
     return response.status(status).json(error)
   } else {
-    console.error(`Error: ${error}`)
+    console.error(`Error: ${error instanceof Object ? JSON.stringify(error) : error}`)
     return response.json(error)
   }
 }
