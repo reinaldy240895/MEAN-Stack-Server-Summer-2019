cf2e7983a4aeaecf4b9f7ce1d3c2359624a8b73b
June Shim <jys2@sfu.ca>
2020-01-18 15:56:59 -0800

    Cleanup unused comments in errorHandler

--- _helpers/error-handler.js
+++ _helpers/error-handler.js
@@ -34,14 +34,3 @@ function errorHandler(err, req, res, next) {
     });
   }
 }
-
-
-// module.exports = handleError = (response, error, status) => {
-//   if (status) {
-//     console.error(`Error(${status}): ${error instanceof Object ? JSON.stringify(error) : error}`)
-//     return response.status(status).json(error)
-//   } else {
-//     console.error(`Error: ${error instanceof Object ? JSON.stringify(error) : error}`)
-//     return response.json(error)
-//   }
-// }
