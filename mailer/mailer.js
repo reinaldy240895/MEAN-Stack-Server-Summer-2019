09086eaccd89d0ae905bf5aa7102ec4224a2c261
June Shim <jys2@sfu.ca>
2020-01-19 02:17:05 -0800

    Change npm package 'dotenv' from devDependencies to dependencies (aws pm2 nginx)

--- mailer/mailer.js
+++ mailer/mailer.js
@@ -1,6 +1,4 @@
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
-}
+require('dotenv').config();
 
 const nodemailer = require('nodemailer');
 const express = require('express');
