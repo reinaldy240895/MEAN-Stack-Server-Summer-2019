11ffdc153da9197a98be8083a909ea6fd187f6c8
June Shim <jys2@sfu.ca>
2020-02-16 15:40:10 -0800

    Revert "Remove use of db.js, mongoose and MongoDB Atlas"
    This reverts commit 65d30bf453b0157e1d8229a3917f9c70408cff85.

--- _helpers/db.js
+++ _helpers/db.js
@@ -1,20 +1,20 @@
-// if (process.env.NODE_ENV !== 'production') {
-//   require('dotenv').config();
-// }
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-// mongoose.connect(process.env.MONGODB_URI, {
-//   useCreateIndex: true,
-//   useUnifiedTopology: true,
-//   useNewUrlParser: true
-// }, () => {
-//   console.log('MongoDB connected successfully!');
-// });
+mongoose.connect(process.env.MONGODB_URI, {
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+}, () => {
+  console.log('MongoDB connected successfully!');
+});
 
-// mongoose.Promise = global.Promise;
+mongoose.Promise = global.Promise;
 
-// module.exports = {
-//   User: require('../users/user.model'),
-//   Post: require('../posts/post.model')
-// };
+module.exports = {
+  User: require('../users/user.model'),
+  Post: require('../posts/post.model')
+};
